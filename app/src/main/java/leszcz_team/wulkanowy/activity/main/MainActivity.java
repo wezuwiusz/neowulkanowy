317535d4e0e2a5d63aff29ee932487ca777fb5d5
RicomenPL <RafalBO99@outlook.com>
2017-03-09 21:29:23 +0100

    Started activity and icon

---
+++ app/src/main/java/leszcz_team/wulkanowy/activity/main/MainActivity.java
@@ -0,0 +1,15 @@
+package leszcz_team.wulkanowy.activity.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import leszcz_team.wulkanowy.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/leszcz_team/wulkanowy/activity/started/LoadingTask.java
@@ -0,0 +1,35 @@
+package leszcz_team.wulkanowy.activity.started;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import leszcz_team.wulkanowy.activity.main.MainActivity;
+
+public class LoadingTask extends AsyncTask<Void, Void, Void> {
+
+    Context activity;
+
+    LoadingTask(Context main) {
+        activity = main;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected void onPostExecute(Void result) {
+
+        Intent intent = new Intent(activity,MainActivity.class);
+        activity.startActivity(intent);
+
+    }
+}
+
