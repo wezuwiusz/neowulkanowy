d2f694d10fdf624ed83752194892480ad193d96f
RicomenPL <rafalbo99@outlook.com>
2017-04-04 20:37:12 +0200

    #1 Add login  POST

--- app/src/main/java/leszcz_team/wulkanowy/activity/main/Login.java
+++ app/src/main/java/leszcz_team/wulkanowy/activity/main/Login.java
@@ -1,11 +1,14 @@
 package leszcz_team.wulkanowy.activity.main;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
-import leszcz_team.wulkanowy.R;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class Login extends AsyncTask<Void, Void, Void> {
 
@@ -13,29 +16,42 @@ public class Login extends AsyncTask<Void, Void, Void> {
     String password;
     String county;
     Activity activity;
+    String test;
 
     public Login(String emailT, String passwordT, String countyT, Activity mainAC){
 
         email = emailT;
         password = passwordT;
-        county = "powiat" + countyT;
+
+        county = "powiat" + countyT.substring(7);
         activity = mainAC;
     }
 
     @Override
     protected Void doInBackground(Void... params) {
+
+        String htmlDefault = "https://cufs.vulcan.net.pl/Default/Account/LogOn";
+
+        try {
+            Connection.Response initial = Jsoup
+                    .connect(htmlDefault)
+                    .data("LoginName", email)
+                    .data("Password", password)
+                    .method(Connection.Method.POST)
+                    .execute();
+
+            Map<String, String> loginCookies = initial.cookies();
+
+        }
+        catch (IOException e){
+            test = e.toString();
+        }
+
         return null;
     }
 
     protected void onPostExecute(Void result) {
         super.onPostExecute(result);
-        new AlertDialog.Builder(activity)
-                .setTitle(R.string.warning_label)
-                .setMessage(R.string.error_feature_text)
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {}
-                })
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .show();
+        Toast.makeText(activity, test, Toast.LENGTH_LONG).show();
     }
 }
