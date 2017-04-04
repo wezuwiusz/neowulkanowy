ad4c53958494051d7b65d3788a147b211c72a99f
RicomenPL <rafalbo99@outlook.com>
2017-04-04 14:18:44 +0200

    Add county and alert dialog

--- app/src/main/java/leszcz_team/wulkanowy/activity/main/MainActivity.java
+++ app/src/main/java/leszcz_team/wulkanowy/activity/main/MainActivity.java
@@ -1,15 +1,45 @@
 package leszcz_team.wulkanowy.activity.main;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import leszcz_team.wulkanowy.R;
 
 public class MainActivity extends Activity {
 
+    private static final String[] COUNTRIES = new String[] {
+            "Jarosławski", "Przeworski"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.warning_label)
+                .setMessage(R.string.warning_text)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {}
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+        autoComplete();
+
+    }
+
+    private void autoComplete(){
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.county_text);
+        textView.setAdapter(adapter);
+
     }
 }
