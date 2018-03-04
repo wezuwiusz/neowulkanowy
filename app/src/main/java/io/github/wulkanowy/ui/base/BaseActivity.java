9f599025bafbe3de498864177a90cb545ae2d927
Mikołaj Pich <m.pich@outlook.com>
2018-03-04 16:35:56 +0100

    [UI] Use different splash background depending on api level  (#54)

--- app/src/main/java/io/github/wulkanowy/ui/base/BaseActivity.java
+++ app/src/main/java/io/github/wulkanowy/ui/base/BaseActivity.java
@@ -3,6 +3,7 @@ package io.github.wulkanowy.ui.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.widget.Toast;
 
 import butterknife.Unbinder;
@@ -22,6 +23,8 @@ public abstract class BaseActivity extends AppCompatActivity implements BaseCont
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
         activityComponent = DaggerActivityComponent.builder()
                 .activityModule(new ActivityModule(this))
