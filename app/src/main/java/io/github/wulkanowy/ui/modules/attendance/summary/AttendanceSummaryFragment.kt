ea6a928cb4c07d20343c3b435f12e478073bf1b0
Mikołaj Pich <m.pich@outlook.com>
2019-01-07 00:56:32 +0100

    Fix spinner dropdown arrow not displaying in attendance summary (#207)

--- app/src/main/java/io/github/wulkanowy/ui/modules/attendance/summary/AttendanceSummaryFragment.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/attendance/summary/AttendanceSummaryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
+import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
@@ -101,7 +102,7 @@ class AttendanceSummaryFragment : BaseSessionFragment(), AttendanceSummaryView, 
     }
 
     override fun showSubjects(show: Boolean) {
-        attendanceSummarySubjects.visibility = if (show) VISIBLE else VISIBLE
+        attendanceSummarySubjectsContainer.visibility = if (show) VISIBLE else INVISIBLE
     }
 
     override fun hideRefresh() {
