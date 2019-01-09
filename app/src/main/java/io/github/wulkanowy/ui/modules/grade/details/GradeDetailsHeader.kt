ed49eb4c9c18f8d8cd119ccb8f45b3ed6e9f0cb5
Rafał Borcz <RafalBO99@outlook.com>
2019-01-09 17:30:31 +0100

    Fix stable id in grade fragment (#211)

--- app/src/main/java/io/github/wulkanowy/ui/modules/grade/details/GradeDetailsHeader.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/grade/details/GradeDetailsHeader.kt
@@ -52,6 +52,8 @@ class GradeDetailsHeader(
         if (subject != other.subject) return false
         if (number != other.number) return false
         if (average != other.average) return false
+        if (newGrades != other.newGrades) return false
+        if (isExpandable != other.isExpandable) return false
 
         return true
     }
@@ -60,6 +62,8 @@ class GradeDetailsHeader(
         var result = subject.hashCode()
         result = 31 * result + number.hashCode()
         result = 31 * result + average.hashCode()
+        result = 31 * result + newGrades
+        result = 31 * result + isExpandable.hashCode()
         return result
     }
 
