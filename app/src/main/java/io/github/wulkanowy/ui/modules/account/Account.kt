a05da2656a3960848e2baef1ce9545e316b6f116
Dominik Korsa <dominik.korsa@gmail.com>
2020-06-12 21:35:51 +0200

    Add account headers in student picker (#871)

---
+++ app/src/main/java/io/github/wulkanowy/ui/modules/account/Account.kt
@@ -0,0 +1,3 @@
+package io.github.wulkanowy.ui.modules.account
+
+data class Account(val email: String, val isParent: Boolean)
--- app/src/main/java/io/github/wulkanowy/ui/modules/account/AccountAdapter.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/account/AccountAdapter.kt
@@ -3,33 +3,72 @@ package io.github.wulkanowy.ui.modules.account
 import android.annotation.SuppressLint
 import android.graphics.PorterDuff
 import android.view.LayoutInflater
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import io.github.wulkanowy.R
 import io.github.wulkanowy.data.db.entities.Student
+import io.github.wulkanowy.databinding.HeaderAccountBinding
 import io.github.wulkanowy.databinding.ItemAccountBinding
+import io.github.wulkanowy.sdk.Sdk
 import io.github.wulkanowy.utils.getThemeAttrColor
 import javax.inject.Inject
 
-class AccountAdapter @Inject constructor() : RecyclerView.Adapter<AccountAdapter.ItemViewHolder>() {
+class AccountAdapter @Inject constructor() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    var items = emptyList<Student>()
+    var items = emptyList<AccountItem<*>>()
 
     var onClickListener: (Student) -> Unit = {}
 
     override fun getItemCount() = items.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ItemViewHolder(
-        ItemAccountBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-    )
+    override fun getItemViewType(position: Int) = items[position].viewType.id
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return when (viewType) {
+            AccountItem.ViewType.HEADER.id -> HeaderViewHolder(HeaderAccountBinding.inflate(inflater, parent, false))
+            AccountItem.ViewType.ITEM.id -> ItemViewHolder(ItemAccountBinding.inflate(inflater, parent, false))
+            else -> throw IllegalStateException()
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is HeaderViewHolder -> bindHeaderViewHolder(holder.binding, items[position].value as Account)
+            is ItemViewHolder -> bindItemViewHolder(holder.binding, items[position].value as Student)
+        }
+    }
+
+    private fun bindHeaderViewHolder(binding: HeaderAccountBinding, account: Account) {
+        with(binding) {
+            accountHeaderEmail.text = account.email
+            accountHeaderType.setText(if (account.isParent) R.string.account_type_parent else R.string.account_type_student)
+        }
+    }
 
     @SuppressLint("SetTextI18n")
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val student = items[position]
-
-        with(holder.binding) {
+    private fun bindItemViewHolder(binding: ItemAccountBinding, student: Student) {
+        with(binding) {
             accountItemName.text = "${student.studentName} ${student.className}"
             accountItemSchool.text = student.schoolName
+            with(accountItemLoginMode) {
+                visibility = when (Sdk.Mode.valueOf(student.loginMode)) {
+                    Sdk.Mode.API -> {
+                        setText(R.string.account_login_mobile_api)
+                        VISIBLE
+                    }
+                    Sdk.Mode.HYBRID -> {
+                        setText(R.string.account_login_hybrid)
+                        VISIBLE
+                    }
+                    Sdk.Mode.SCRAPPER -> {
+                        GONE
+                    }
+                }
+            }
 
             with(accountItemImage) {
                 val colorImage = if (student.isCurrent) context.getThemeAttrColor(R.attr.colorPrimary)
@@ -41,6 +80,9 @@ class AccountAdapter @Inject constructor() : RecyclerView.Adapter<AccountAdapter
             root.setOnClickListener { onClickListener(student) }
         }
     }
+
+    class HeaderViewHolder(val binding: HeaderAccountBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     class ItemViewHolder(val binding: ItemAccountBinding) : RecyclerView.ViewHolder(binding.root)
 }
