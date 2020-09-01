69a11931547358f030330fac4f9a49e4ba74603b
Mikołaj Pich <m.pich@outlook.com>
2020-09-01 23:58:18 +0200

    Fix semester list refresh on no current semester found (#940)

--- app/src/main/java/io/github/wulkanowy/data/repositories/semester/SemesterRepository.kt
+++ app/src/main/java/io/github/wulkanowy/data/repositories/semester/SemesterRepository.kt
@@ -1,5 +1,6 @@
 package io.github.wulkanowy.data.repositories.semester
 
+import io.github.wulkanowy.data.db.entities.Semester
 import io.github.wulkanowy.data.db.entities.Student
 import io.github.wulkanowy.sdk.Sdk
 import io.github.wulkanowy.utils.DispatchersProvider
@@ -18,24 +19,33 @@ class SemesterRepository @Inject constructor(
 ) {
 
     suspend fun getSemesters(student: Student, forceRefresh: Boolean = false, refreshOnNoCurrent: Boolean = false) = withContext(dispatchers.backgroundThread) {
-        local.getSemesters(student).let { semesters ->
-            semesters.filter {
-                !forceRefresh && when {
-                    Sdk.Mode.valueOf(student.loginMode) != Sdk.Mode.API -> semesters.firstOrNull { it.isCurrent }?.diaryId != 0
-                    refreshOnNoCurrent -> semesters.any { semester -> semester.isCurrent }
-                    else -> true
-                }
-            }
-        }.ifEmpty {
-            val new = remote.getSemesters(student)
-            if (new.isEmpty()) throw IllegalArgumentException("Empty semester list!")
+        val semesters = local.getSemesters(student)
 
-            val old = local.getSemesters(student)
-            local.deleteSemesters(old.uniqueSubtract(new))
-            local.saveSemesters(new.uniqueSubtract(old))
-
+        if (isShouldFetch(student, semesters, forceRefresh, refreshOnNoCurrent)) {
+            refreshSemesters(student)
             local.getSemesters(student)
-        }
+        } else semesters
+    }
+
+    private fun isShouldFetch(student: Student, semesters: List<Semester>, forceRefresh: Boolean, refreshOnNoCurrent: Boolean): Boolean {
+        val isNoSemesters = semesters.isEmpty()
+
+        val isRefreshOnModeChangeRequired = if (Sdk.Mode.valueOf(student.loginMode) != Sdk.Mode.API) {
+            semesters.firstOrNull { it.isCurrent }?.diaryId == 0
+        } else false
+
+        val isRefreshOnNoCurrentAppropriate = refreshOnNoCurrent && !semesters.any { semester -> semester.isCurrent }
+
+        return forceRefresh || isNoSemesters || isRefreshOnModeChangeRequired || isRefreshOnNoCurrentAppropriate
+    }
+
+    private suspend fun refreshSemesters(student: Student) {
+        val new = remote.getSemesters(student)
+        if (new.isEmpty()) throw IllegalArgumentException("Empty semester list!")
+
+        val old = local.getSemesters(student)
+        local.deleteSemesters(old.uniqueSubtract(new))
+        local.saveSemesters(new.uniqueSubtract(old))
     }
 
     suspend fun getCurrentSemester(student: Student, forceRefresh: Boolean = false) = withContext(dispatchers.backgroundThread) {
