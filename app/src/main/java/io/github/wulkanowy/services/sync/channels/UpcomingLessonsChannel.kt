90be9d1add6ad1f92c90b6125e91b77dd9a49625
Mateusz Idziejczak <idziejczak3@gmail.com>
2020-09-01 12:57:45 +0200

    Disable notification sound (#936)

--- app/src/main/java/io/github/wulkanowy/services/sync/channels/UpcomingLessonsChannel.kt
+++ app/src/main/java/io/github/wulkanowy/services/sync/channels/UpcomingLessonsChannel.kt
@@ -26,6 +26,7 @@ class UpcomingLessonsChannel @Inject constructor(
                 lockscreenVisibility = VISIBILITY_PUBLIC
                 setShowBadge(false)
                 enableVibration(false)
+                setSound(null, null)
             }
         )
     }
