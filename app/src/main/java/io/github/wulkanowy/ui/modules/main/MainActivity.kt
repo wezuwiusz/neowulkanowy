ff425d6d2bc52217e7ab168db76e21e5c29e9557
Damian Czupryn <60961958+Daxxxis@users.noreply.github.com>
2021-02-18 00:05:27 +0100

    Change background color of navigation and notification bars (#1120)

--- app/src/main/java/io/github/wulkanowy/ui/modules/main/MainActivity.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/main/MainActivity.kt
@@ -11,6 +11,7 @@ import android.graphics.drawable.Icon
 import android.os.Build
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.LOLLIPOP
+import android.os.Build.VERSION_CODES.P
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -194,6 +195,7 @@ class MainActivity : BaseActivity<MainPresenter, ActivityMainBinding>(), MainVie
         return true
     }
 
+    @SuppressLint("NewApi")
     override fun initView() {
         with(binding.mainToolbar) {
             if (SDK_INT >= LOLLIPOP) stateListAnimator = null
@@ -233,12 +235,18 @@ class MainActivity : BaseActivity<MainPresenter, ActivityMainBinding>(), MainVie
 
         with(navController) {
             setOnViewChangeListener { section, name ->
-                binding.mainBottomNav.visibility =
-                    if (section == MainView.Section.ACCOUNT || section == MainView.Section.STUDENT_INFO) {
-                        View.GONE
-                    } else {
-                        View.VISIBLE
+                if (section == MainView.Section.ACCOUNT || section == MainView.Section.STUDENT_INFO) {
+                    binding.mainBottomNav.visibility = View.GONE
+                    if (appInfo.systemVersion >= P) {
+                        window.navigationBarColor = getThemeAttrColor(R.attr.colorSurface)
                     }
+                } else {
+                    binding.mainBottomNav.visibility = View.VISIBLE
+                    if (appInfo.systemVersion >= P) {
+                        window.navigationBarColor =
+                            getThemeAttrColor(android.R.attr.navigationBarColor)
+                    }
+                }
 
                 analytics.setCurrentScreen(this@MainActivity, name)
                 presenter.onViewChange(section)
