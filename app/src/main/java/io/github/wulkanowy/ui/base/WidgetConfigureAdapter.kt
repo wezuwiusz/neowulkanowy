e03b0dfa0150505da6c67c9bc4917ed292719e66
Rafał Borcz <RafalBO99@outlook.com>
2021-03-21 12:04:55 +0100

    Fix missing avatars in widgets (#1238)

--- app/src/main/java/io/github/wulkanowy/ui/base/WidgetConfigureAdapter.kt
+++ app/src/main/java/io/github/wulkanowy/ui/base/WidgetConfigureAdapter.kt
@@ -1,18 +1,20 @@
 package io.github.wulkanowy.ui.base
 
 import android.annotation.SuppressLint
-import android.graphics.PorterDuff
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import io.github.wulkanowy.R
 import io.github.wulkanowy.data.db.entities.Student
 import io.github.wulkanowy.databinding.ItemAccountBinding
+import io.github.wulkanowy.utils.createNameInitialsDrawable
 import io.github.wulkanowy.utils.getThemeAttrColor
 import io.github.wulkanowy.utils.nickOrName
 import javax.inject.Inject
 
-class WidgetConfigureAdapter @Inject constructor() : RecyclerView.Adapter<WidgetConfigureAdapter.ItemViewHolder>() {
+class WidgetConfigureAdapter @Inject constructor() :
+    RecyclerView.Adapter<WidgetConfigureAdapter.ItemViewHolder>() {
 
     var items = emptyList<Pair<Student, Boolean>>()
 
@@ -27,16 +29,31 @@ class WidgetConfigureAdapter @Inject constructor() : RecyclerView.Adapter<Widget
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val (student, isCurrent) = items[position]
+        val context = holder.binding.root.context
+        val checkBackgroundColor = context.getThemeAttrColor(R.attr.colorSurface)
+        val avatar = context.createNameInitialsDrawable(student.nickOrName, student.avatarColor)
+        val isDuplicatedStudent = items.filter {
+            val studentToCompare = it.first
+
+            studentToCompare.studentId == student.studentId
+                && studentToCompare.schoolSymbol == student.schoolSymbol
+                && studentToCompare.symbol == student.symbol
+        }.size > 1
 
         with(holder.binding) {
             accountItemName.text = "${student.nickOrName} ${student.className}"
             accountItemSchool.text = student.schoolName
+            accountItemImage.setImageDrawable(avatar)
 
-            with(accountItemImage) {
-                val colorImage = if (isCurrent) context.getThemeAttrColor(R.attr.colorPrimary)
-                else context.getThemeAttrColor(R.attr.colorOnSurface, 153)
+            with(accountItemAccountType) {
+                setText(if (student.isParent) R.string.account_type_parent else R.string.account_type_student)
+                isVisible = isDuplicatedStudent
+            }
 
-                setColorFilter(colorImage, PorterDuff.Mode.SRC_IN)
+            with(accountItemCheck) {
+                isVisible = isCurrent
+                borderColor = checkBackgroundColor
+                circleColor = checkBackgroundColor
             }
 
             root.setOnClickListener { onClickListener(student) }
