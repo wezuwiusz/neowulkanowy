b2efe0d981c2890859b24e4ae0123cbe46a7c63b
Mateusz Idziejczak <idziejczak3@gmail.com>
2021-05-02 12:33:27 +0200

    Probably fix list divider color (#1270)
    Co-authored-by: Rafał Borcz <RafalBO99@outlook.com>

--- app/src/main/java/io/github/wulkanowy/ui/widgets/DividerItemDecoration.kt
+++ app/src/main/java/io/github/wulkanowy/ui/widgets/DividerItemDecoration.kt
@@ -2,7 +2,6 @@ package io.github.wulkanowy.ui.widgets
 
 import android.content.Context
 import android.graphics.Canvas
-import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,13 +11,13 @@ class DividerItemDecoration(context: Context) : DividerItemDecoration(context, V
         canvas.save()
         val dividerLeft = parent.paddingLeft
         val dividerRight = parent.width - parent.paddingRight
-        val childCount = parent.childCount
 
-        for (i in 0..childCount - 2) {
-            val child: View = parent.getChildAt(i)
+        for (i in 0..parent.childCount - 2) {
+            val child = parent.getChildAt(i)
             val params = child.layoutParams as RecyclerView.LayoutParams
-            val dividerTop: Int = child.bottom + params.bottomMargin
+            val dividerTop = child.bottom + params.bottomMargin
             val dividerBottom = dividerTop + drawable!!.intrinsicHeight
+
             drawable?.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom)
             drawable?.draw(canvas)
         }
