60501fcd728bae993c110a4ed500b5dd66012ae0
Mikołaj Pich <m.pich@outlook.com>
2021-10-03 14:13:42 +0200

    Set buildTimestamp through manifest meta (#1556)

--- app/src/main/java/io/github/wulkanowy/utils/AppInfo.kt
+++ app/src/main/java/io/github/wulkanowy/utils/AppInfo.kt
@@ -1,10 +1,12 @@
 package io.github.wulkanowy.utils
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.os.Build.MANUFACTURER
 import android.os.Build.MODEL
 import android.os.Build.VERSION.SDK_INT
-import io.github.wulkanowy.BuildConfig.BUILD_TIMESTAMP
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.github.wulkanowy.BuildConfig.DEBUG
 import io.github.wulkanowy.BuildConfig.FLAVOR
 import io.github.wulkanowy.BuildConfig.VERSION_CODE
@@ -13,13 +15,21 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-open class AppInfo @Inject constructor() {
+open class AppInfo @Inject constructor(
+    @ApplicationContext private val context: Context,
+) {
 
     open val isDebug get() = DEBUG
 
     open val versionCode get() = VERSION_CODE
 
-    open val buildTimestamp get() = BUILD_TIMESTAMP
+    open val buildTimestamp: Long
+        get() {
+            val info = context.packageManager.getApplicationInfo(
+                context.packageName, PackageManager.GET_META_DATA,
+            )
+            return info.metaData?.getFloat("buildTimestamp")?.toLong() ?: 0
+        }
 
     open val buildFlavor get() = FLAVOR
 
--- app/src/test/java/io/github/wulkanowy/data/db/migrations/AbstractMigrationTest.kt
+++ app/src/test/java/io/github/wulkanowy/data/db/migrations/AbstractMigrationTest.kt
@@ -16,6 +16,8 @@ abstract class AbstractMigrationTest {
 
     val dbName = "migration-test"
 
+    val context: Context get() = ApplicationProvider.getApplicationContext<Context>()
+
     @get:Rule
     val helper: MigrationTestHelper = MigrationTestHelper(
         InstrumentationRegistry.getInstrumentation(),
@@ -24,7 +26,6 @@ abstract class AbstractMigrationTest {
     )
 
     fun getMigratedRoomDatabase(): AppDatabase {
-        val context = ApplicationProvider.getApplicationContext<Context>()
         val database = Room.databaseBuilder(
             ApplicationProvider.getApplicationContext(),
             AppDatabase::class.java,
@@ -32,7 +33,7 @@ abstract class AbstractMigrationTest {
         ).addMigrations(
             *AppDatabase.getMigrations(
                 SharedPrefProvider(PreferenceManager.getDefaultSharedPreferences(context)),
-                AppInfo()
+                AppInfo(context)
             )
         ).build()
         // close the database and release any stream resources when the test finishes
