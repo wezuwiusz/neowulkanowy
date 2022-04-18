426bee882c8d4aac502b99bdbee24c5ca527c2e2
Mikołaj Pich <m.pich@outlook.com>
2022-04-18 16:52:28 +0200

    Display timetable header as HTML on dashboard tile (#1835)

--- app/src/main/java/io/github/wulkanowy/ui/modules/dashboard/DashboardAdapter.kt
+++ app/src/main/java/io/github/wulkanowy/ui/modules/dashboard/DashboardAdapter.kt
@@ -9,6 +9,7 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.text.parseAsHtml
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.core.view.updateMarginsRelative
@@ -563,7 +564,7 @@ class DashboardAdapter @Inject constructor() : RecyclerView.Adapter<RecyclerView
     ) {
         with(binding.dashboardLessonsItemDayHeader) {
             isVisible = header != null
-            text = header?.content
+            text = header?.content?.parseAsHtml()
         }
     }
 
