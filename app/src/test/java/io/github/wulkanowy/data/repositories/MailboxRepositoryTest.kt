6153c7b97d58f36383f05d4357194c9c3dcb7720
Mikołaj Pich <m.pich@outlook.com>
2022-09-01 14:55:00 +0200

    Add support for match mailboxes with more different names (#1961)

--- app/src/test/java/io/github/wulkanowy/data/repositories/MailboxRepositoryTest.kt
+++ app/src/test/java/io/github/wulkanowy/data/repositories/MailboxRepositoryTest.kt
@@ -77,18 +77,74 @@ class MailboxRepositoryTest {
         assertEquals(expectedMailbox, selectedMailbox)
     }
 
-    @Test(expected = IllegalArgumentException::class)
-    fun `get mailbox for student with strange name`() = runTest {
+    @Test
+    fun `get mailbox for unique non-authorized student`() = runTest {
         val student = getStudentEntity(
             userName = "Stanisław Kowalski",
-            studentName = "J**** K*****",
+            studentName = "J** K*******",
         )
         val expectedMailbox = getMailboxEntity("Jan Kowalski")
         coEvery { mailboxDao.loadAll(any()) } returns listOf(
             expectedMailbox,
         )
 
+        assertEquals(expectedMailbox, systemUnderTest.getMailbox(student))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `get mailbox for not-unique non-authorized student`() = runTest {
+        val student = getStudentEntity(
+            userName = "Stanisław Kowalski",
+            studentName = "J** K*******",
+        )
+        coEvery { mailboxDao.loadAll(any()) } returns listOf(
+            getMailboxEntity("Jan Kowalski"),
+            getMailboxEntity("Jan Kurowski"),
+        )
+
         systemUnderTest.getMailbox(student)
+    }
+
+    @Test
+    fun `get mailbox for student with uppercase name`() = runTest {
+        val student = getStudentEntity(
+            userName = "Mochoń Julia",
+            studentName = "KLAUDIA MOCHOŃ",
+        )
+        val expectedMailbox = getMailboxEntity("Klaudia Mochoń")
+        coEvery { mailboxDao.loadAll(any()) } returns listOf(
+            expectedMailbox,
+        )
+
+        assertEquals(expectedMailbox, systemUnderTest.getMailbox(student))
+    }
+
+    @Test
+    fun `get mailbox for student with second name`() = runTest {
+        val student = getStudentEntity(
+            userName = "Fistaszek Karolina",
+            studentName = "Julia Fistaszek",
+        )
+        val expectedMailbox = getMailboxEntity("Julia Maria Fistaszek")
+        coEvery { mailboxDao.loadAll(any()) } returns listOf(
+            expectedMailbox,
+        )
+
+        assertEquals(expectedMailbox, systemUnderTest.getMailbox(student))
+    }
+
+    @Test
+    fun `get mailbox for student with second name and uppercase`() = runTest {
+        val student = getStudentEntity(
+            userName = "BEDNAREK KAMIL",
+            studentName = "ALEKSANDRA BEDNAREK",
+        )
+        val expectedMailbox = getMailboxEntity("Aleksandra Anna Bednarek")
+        coEvery { mailboxDao.loadAll(any()) } returns listOf(
+            expectedMailbox,
+        )
+
+        assertEquals(expectedMailbox, systemUnderTest.getMailbox(student))
     }
 
     private fun getMailboxEntity(
