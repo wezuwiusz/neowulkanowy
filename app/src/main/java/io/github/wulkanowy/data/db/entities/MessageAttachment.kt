a495fcbc5f30decab849c0c9a7f76000192d91a5
Mikołaj Pich <m.pich@outlook.com>
2023-03-02 18:01:48 +0100

    Fix saving attachements with same url but from different messages (#2137)

--- app/src/main/java/io/github/wulkanowy/data/db/entities/MessageAttachment.kt
+++ app/src/main/java/io/github/wulkanowy/data/db/entities/MessageAttachment.kt
@@ -2,15 +2,13 @@ package io.github.wulkanowy.data.db.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import java.io.Serializable
 
-@Entity(tableName = "MessageAttachments")
+@Entity(
+    tableName = "MessageAttachments",
+    primaryKeys = ["message_global_key", "url", "filename"],
+)
 data class MessageAttachment(
-
-    @PrimaryKey
-    @ColumnInfo(name = "real_id")
-    val realId: Int,
 
     @ColumnInfo(name = "message_global_key")
     val messageGlobalKey: String,
