85d3a6e8a7b8290b4ebd5fab7cf0394525601e2c
Mikołaj Pich <m.pich@outlook.com>
2024-05-29 16:33:49 +0200

    Bump sdk to 2.6.12-SNAPSHOT

--- app/src/main/java/io/github/wulkanowy/data/api/services/WulkanowyService.kt
+++ app/src/main/java/io/github/wulkanowy/data/api/services/WulkanowyService.kt
@@ -11,6 +11,6 @@ interface WulkanowyService {
     @GET("/v1.json")
     suspend fun getAdminMessages(): List<AdminMessage>
 
-    @GET("/mapping2.json")
+    @GET("/mapping3.json")
     suspend fun getMapping(): Mapping
 }
